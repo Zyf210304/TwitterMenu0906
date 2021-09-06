deb146c8afc548145d36ea137611239bcc7b070c
张亚飞 <672233580@qq.com>
2021-09-06 17:04:19 +0800

    Initial Commit

---
+++ TwitterMenu0906/TwitterMenu0906App.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterMenu0906App.swift
+//  TwitterMenu0906
+//
+//  Created by 张亚飞 on 2021/9/6.
+//
+
+import SwiftUI
+
+@main
+struct TwitterMenu0906App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
